8c7e234a53ed3935b9368d2adc75ccd6c6fd9404
Damar Wisnu Candra Prabowo <damar.wisnu1@gmail.com>
2022-08-25 12:05:54 +0700

    ketiga

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,10 +2,11 @@
 <html lang="en">
 <head>
   <!-- Title -->
-  <title>Classic Portfolio | Front - Responsive Website Template</title>
-
-  <!-- Required Meta Tags Always Come First -->
+  <link rel="shortcut icon" href="{{ asset('image/pemda.ico')}}">
+  <title>URL SHORTENER KABUPATEN WONOSOBO</title>
   <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="csrf-token" content="{{csrf_token()}}">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Favicon -->
