04728b43a4b4d95622687a185674882bc4a3759f
Damar Wisnu Candra Prabowo <damar.wisnu1@gmail.com>
2022-09-20 13:57:59 +0700

    penambahan meta di notfound

--- resources/views/notfound.blade.php
+++ resources/views/notfound.blade.php
@@ -1,7 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="{{ asset('image/pemda.ico')}}">
+    <title>URL SHORTENER KABUPATEN WONOSOBO</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="csrf-token" content="{{csrf_token()}}">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <style>
 body, html {
   height: 100%;
